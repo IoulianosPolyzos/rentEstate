2ecb5adc8d88e874daada98a91f3117790cdf06e
IoulianosPolyzos <ioulianospol@gmail.com>
2025-06-04 20:04:26 +0300

    port to 8081

--- src/test/java/gr/hua/dit/rentEstate/RentEstatesApplicationTests.java
+++ src/test/java/gr/hua/dit/rentEstate/RentEstatesApplicationTests.java
@@ -9,5 +9,19 @@ class RentEstatesApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void testCreateUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"email\":\"api@hua.gr\",\"password\":\"pass123\"}";
+
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk());
+	}
 
 }
